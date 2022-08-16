98f0c37c2ee591c055599f8ca329976a5327fb43
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-08-16 16:56:14 -0500

    helper join table and indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -23,4 +23,25 @@ CREATE TABLE invoice_items(
     total_price DECIMAL(5,2),
     invoice_id INT REFERENCES invoices (id),
     treatment_id INT REFERENCES treatments (id)
-    );
+);
+
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    total_amount DECIMAL(5,2),
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT REFERENCES medical_histories(id)
+);    
+
+
+CREATE TABLE medication (
+    treatment_id INT REFERENCES treatments(id),
+    medical_history_id INT REFERENCES medical_histories(id)
+);
+
+CREATE INDEX index_patient_id ON medical_histories(patient_id);
+CREATE INDEX index_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX index_treatment_id ON invoice_items(treatment_id);
+CREATE INDEX index_medical_history_id ON invoices(medical_history_id);
+CREATE INDEX index_treatment_id_medication ON medication(treatment_id);
+CREATE INDEX index_medical_history_id_medication ON medication(medical_history_id);
