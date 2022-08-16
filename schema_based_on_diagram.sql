a79f590ab517997137a8e13f8b614c9720353b8e
Menanya <emmmiemenz@gmail.com>
2022-08-16 22:31:22 +0100

    add treatments and invoice_item teble

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,4 +9,18 @@ CREATE TABLE medical_histories (
     admitted_at TIMESTAMP,
     patient_id INT REFERENCES patients(id),
     status VARCHAR(100)
-)
+);
+
+CREATE TABLE treatments(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    type VARCHAR(50),
+    name VARCHAR (100)
+);
+CREATE TABLE invoice_items(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    unit_price DECIMAL(5,2),
+    quantity INT,
+    total_price DECIMAL(5,2),
+    invoice_id INT REFERENCES invoices (id),
+    treatment_id INT REFERENCES treatments (id)
+    );
